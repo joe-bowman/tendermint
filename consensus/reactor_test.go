efb80c741b35ab016c5348f7d366b9dc9717a6c0
Anton Kaliaev <anton.kalyaev@gmail.com>
2018-12-01 11:58:00 +0100

    Review comments: @melekes suggestions
    require instead of assert
    
    Co-Authored-By: Liamsi <Ismail.Khoffi@gmail.com>

--- consensus/reactor_test.go
+++ consensus/reactor_test.go
@@ -282,7 +282,7 @@ func TestReactorVotingPowerChange(t *testing.T) {
 	activeVals := make(map[string]struct{})
 	for i := 0; i < nVals; i++ {
 		addr, err := css[i].privValidator.GetAddress()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		activeVals[string(addr)] = struct{}{}
 	}
 
